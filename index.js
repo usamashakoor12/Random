768618fef60052f3a03f8ed41c4cf334381f1571
Usama Shakoor <shakoorqariusama@gmail.com>
2024-07-23 14:44:59 +0500

    01/first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('Code Below');
+
+const min = 1;
+const max = 6236;
+
+console.log(min,max);
+
+function rendom(max) {
+      return  (Math.floor(Math.random()*max + 1))     
+}
+console.log(rendom(max));
